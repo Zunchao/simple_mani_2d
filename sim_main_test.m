326c53fb94ce55122ef97acd87cf4702da77acfe
zzc <zunchao.zheng@student.kit.edu>
2017-02-08 17:49:50 +0100

    create new doc, test trans matrics for simple arm, plot workspace & jointspace of 1-2-3 joints arm

---
+++ sim_main_test.m
@@ -0,0 +1,36 @@
+function [ output_args ] = sim_main_test()
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+ni = 5;
+nj = 21;
+l_joint = 10;
+
+theta_1_joint = linspace(-pi/2,pi/2,ni);
+theta_2_joint = linspace(-pi/2,pi/2,nj);
+
+for i = 1:ni
+    T1 = trans_Joint( theta_1_joint(i), l_joint );
+    px1 = [0, T1(1,end)];
+    py1 = [0, T1(2,end)];
+    plot(T1(1,end), T1(2,end), 'bo')
+    axis([-l_joint*3 l_joint*3 -l_joint*3 l_joint*3])
+    hold on
+    for j = 1:nj
+        T12 = trans_Joint( theta_2_joint(j), l_joint );
+        T2 = T1*T12;
+        px2 = [T1(1,end), T2(1,end)];
+        py2 = [T1(2,end), T2(2,end)];
+        plot(T2(1,end), T2(2,end), 'r.')
+        
+        for k = 1:nj
+            T13 = trans_Joint( theta_2_joint(k), l_joint );
+            T3 = T2*T13;
+            px2 = [T2(1,end), T3(1,end)];
+            py2 = [T2(2,end), T3(2,end)];
+            plot(T3(1,end), T3(2,end), 'g.')
+            %drawnow
+        end
+    end
+end
+end
+
---
+++ trans_Joint.m
@@ -0,0 +1,17 @@
+function T = trans_Joint( Theta, L )
+% TRANSFORM MATRIX FOR ON JOINT
+%{ 
+R1 = [cos(Theta) sin(Theta) 0;
+    -sin(Theta) cos(Theta) 0;
+    0 0 1];
+T2 = [1 0 0;
+    0 1 L;
+    0 0 1];
+
+T = R1*T2;
+%}
+T = [cos(Theta) sin(Theta) L*sin(Theta);
+    -sin(Theta) cos(Theta) L*cos(Theta);
+    0 0 1];
+end
+
